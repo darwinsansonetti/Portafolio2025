3fe72b23cc8c0f4f0a1cfa0848047ba7fb56acc1
Darwin Sansonetti <sansonettidiazdarwinjose@gmail.com>
2025-07-02 19:40:08 -0400

    Subida inicial del portafolio Laravel

---
+++ resources/js/adsterra728x90.js
@@ -0,0 +1,16 @@
+var atOptions = {
+    key: '4ed08ebc262598044bdab6ac2cfaeae4',
+    format: 'iframe',
+    height: 90,
+    width: 728,
+    params: {}
+};
+
+(function() {
+    var script = document.createElement('script');
+    script.src = "//soulsdeliriousnoise.com/4ed08ebc262598044bdab6ac2cfaeae4/invoke.js";
+    script.type = 'text/javascript';
+    script.async = true;
+    var node = document.getElementsByTagName('script')[0];
+    node.parentNode.insertBefore(script, node);
+})();
